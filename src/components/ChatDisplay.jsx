3964ef968df139bed4fde9396e956769251d0374
Carlos Melero <cmelero96@gmail.com>
2022-05-25 13:20:31 +0200

    Added ChatDisplay unit tests

--- src/components/ChatDisplay.jsx
+++ src/components/ChatDisplay.jsx
@@ -3,6 +3,16 @@ import { COMMAND_DATA } from '../constants/commands';
 
 import './ChatDisplay.css';
 
+const getDataTestId = (msg) => {
+  if (msg.command === COMMAND_DATA.think.key) {
+    if (msg.ownedByCurrentUser) return 'thought-message-mine';
+    else return 'thought-message-theirs';
+  } else {
+    if (msg.ownedByCurrentUser) return 'message-mine';
+    else return 'message-theirs';
+  }
+}
+
 const ChatDisplay = ({messages}) => {
   return (
     <div className="messages-container">
@@ -10,6 +20,7 @@ const ChatDisplay = ({messages}) => {
         {messages.map((message, i) => (
           <li
             key={i}
+            data-testid={getDataTestId(message)}
             className={`message-item ${
               message.ownedByCurrentUser ? 'my-message' : 'received-message'
             } ${message.command === COMMAND_DATA.think.key ? 'message-thought' : ''}`}
---
+++ src/components/ChatDisplay.test.js
@@ -0,0 +1,62 @@
+/* eslint-disable testing-library/no-node-access */
+import {fireEvent, render, screen} from '@testing-library/react';
+import ChatDisplay from './ChatDisplay';
+import {COMMAND_DATA} from '../constants/commands';
+
+describe('ChatDisplay', () => {
+  const MOCK_MESSAGES = [
+    {
+      body: 'My message 1',
+      ownedByCurrentUser: true,
+    },
+    {
+      body: 'My message 2',
+      ownedByCurrentUser: true,
+    },
+    {
+      body: 'My message 3',
+      ownedByCurrentUser: true,
+    },
+    {
+      body: 'My thought',
+      command: 'think',
+      ownedByCurrentUser: true,
+    },
+    {
+      body: 'Their message',
+      ownedByCurrentUser: false,
+    },
+    {
+      body: 'Their thought',
+      command: 'think',
+      ownedByCurrentUser: false,
+    },
+  ];
+
+  const setup = () => {
+    render(<ChatDisplay messages={MOCK_MESSAGES} />);
+  };
+
+  beforeEach(() => setup());
+
+  it('Displays messages and thoughts classified correctly', () => {
+    const myMessages = screen.getAllByTestId('message-mine');
+    const myThoughts = screen.getAllByTestId('thought-message-mine');
+    const theirMessages = screen.getAllByTestId('message-theirs');
+    const theirThoughts = screen.getAllByTestId('thought-message-theirs');
+
+    const myMockMessages = MOCK_MESSAGES.filter((m) => m.ownedByCurrentUser && !m.command);
+    const myMockThoughts = MOCK_MESSAGES.filter(
+      (m) => m.ownedByCurrentUser && m.command === COMMAND_DATA.think.key
+    );
+    const theirMockMessages = MOCK_MESSAGES.filter((m) => !m.ownedByCurrentUser && !m.command);
+    const theirMockThoughts = MOCK_MESSAGES.filter(
+      (m) => !m.ownedByCurrentUser && m.command === COMMAND_DATA.think.key
+    );
+
+    expect(myMessages).toHaveLength(myMockMessages.length);
+    expect(myThoughts).toHaveLength(myMockThoughts.length);
+    expect(theirMessages).toHaveLength(theirMockMessages.length);
+    expect(theirThoughts).toHaveLength(theirMockThoughts.length);
+  });
+});
